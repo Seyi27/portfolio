3f9e32c1bbb41c13ace8d1cbfec09c578a6aa402
seyi27 <oluwaseyibhadmus@gmail.com>
2023-08-22 23:24:44 +0100

    navbar update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -60,7 +60,7 @@ function Navbar() {
         {toggle && (
           <motion.div
             whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.85, ease: easeOut }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
           >
             <HiX onClick={() => setToggle(false)} />
             <ul>
